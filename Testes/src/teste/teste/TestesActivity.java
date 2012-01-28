19fdac7999a58d569e179606d6702666d0fc8084
barenko <barenko@gmail.com>
2012-01-28 17:03:48 -0200

    testes

---
+++ Testes/src/teste/teste/TestesActivity.java
@@ -0,0 +1,13 @@
+package teste.teste;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
